19ccaf535aa895ecc1c33648feff535500c55142
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-07-21 19:11:23 +0200

    added 3d model and colors

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -3,7 +3,6 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 import { useEffect, useRef, useState } from "react";
-
 import { hightlightsSlides } from "../constants";
 import { pauseImg, playImg, replayImg } from "../utils";
 
